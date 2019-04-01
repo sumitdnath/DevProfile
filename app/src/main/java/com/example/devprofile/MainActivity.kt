3a1d41a0e4c7d62d3dd0ff25c19b89561009aaca
Sumit <thunder11h@gmail.com>
2019-04-01 11:31:39 +0530

    layout changes

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.devprofile
 
+import android.content.res.Resources
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        rounded.cornerRadius = 15f
+//       rounded.isCircular = true
+        logo.setImageDrawable(rounded)
+
     }
 }
